f493d99fd6b792e91150f764b1e56181129de833
NOEMI GP <noemigp0@gmail.com>
2023-12-17 21:35:19 -0600

    Adding final guest list version

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,20 @@ public class Main {
             int option = getOption();
 
             if (option == 1) {
+
                 addGuest();
 
             } else if (option == 2) {
+
                 removeGuest();
 
-            } else if (option == 3) {
+            } else if (option == 3){
+                renameGuest();
+
+            } else if (option == 4) {
+                addGuestSpecificPosition();
+
+            } else if(option == 5){
                 System.out.println("Exiting...");
                 break;
             }
@@ -49,12 +57,16 @@ public class Main {
         System.out.println("______________________________\n- Menu -");
         System.out.println("1 - Add Guest");
         System.out.println("2 - Remove Guest");
-        System.out.println("3 - Exit");
+        System.out.println("3 - Rename Guest");
+        System.out.println("4 - Add guest in position");
+        System.out.println("5 - Exit");
         System.out.print("Option: ");
     }
 
     static int getOption() {
         int option = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
         return option;
     }
 
@@ -63,7 +75,7 @@ public class Main {
         for (int i = 0; i < guests.length; i++) {
             if (guests[i] == null) {
                 System.out.print("Name: ");
-                guests[i] = scanner.next();
+                guests[i] = scanner.nextLine();
 
                 break;
             }
@@ -76,6 +88,10 @@ public class Main {
         System.out.print("Guest number: ");
         int guestNumber = scanner.nextInt();
         int flag = 0;
+
+        if(guestNumber > 0 && guestNumber < 10 && guests[guestNumber -1] != null){
+
+
         for (int i = 0; i < guests.length; i++) {
             if ( guests[i] == guests[guestNumber-1]) {
                 guests[i] = null;
@@ -92,10 +108,14 @@ public class Main {
 
 
         }
+
+        } else {
+            System.out.println("Error. There is no guest with that number");
+        }
     }
 
     static void insertTestNames() {
-        guests[0] = "Jacob";
+        guests[0] = "Jacob Hughes";
         guests[1] = "Edward";
         guests[2] = "Rose";
         guests[3] = "Molly";
@@ -105,6 +125,45 @@ public class Main {
         guests[7] = "Isabel";
         guests[8] = "Misael";
         guests[9] = "Noe";*/
+
+    }
+
+    static void renameGuest(){
+        System.out.println("Guest number:");
+        int guestNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+
+        if(guestNumber > 0 && guestNumber < 10 && guests[guestNumber -1] != null) {
+            System.out.println("New name: ");
+            String newName = scanner.nextLine();
+
+
+            guests[guestNumber - 1] = newName;
+        } else {
+            System.out.println("Error. There is no guest with that number");
+        }
+
+    }
+
+    static void addGuestSpecificPosition(){
+
+        System.out.println("Guest number:");
+        int guestNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+
+        if(guestNumber > 0 && guestNumber < 10 && guests[guestNumber -1] != null) {
+            System.out.println("New name: ");
+            String newName = scanner.nextLine();
+
+            for (int i = guests.length -1; i > guestNumber -1 ; i--){
+                guests[i] = guests[i-1];
+            }
+            guests[guestNumber - 1] = newName;
+        } else {
+            System.out.println("Error. There is no guest with that number");
+        }
 
     }
 }
